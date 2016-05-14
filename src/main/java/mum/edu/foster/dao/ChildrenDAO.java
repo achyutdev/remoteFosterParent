d6bc4c6f2986e5fdc1b7844bdccdda423532f637
Kibrom1 <kibmit@gmail.com>
2016-05-14 17:34:21 -0500

    children service and dao added

--- src/main/java/mum/edu/foster/dao/ChildrenDAO.java
+++ src/main/java/mum/edu/foster/dao/ChildrenDAO.java
@@ -2,13 +2,25 @@ package mum.edu.foster.dao;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import mum.edu.foster.domain.Children;
 import mum.edu.foster.domain.Person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChildrenDAO extends JpaRepository<Person, Long> {
 
 	public List<Person> findAll();
 
 	public List<Person> findByDob();
+
+	public Person findById(long id);
+
+	public List<Person> findByFirstName(String firstName);
+
+	public List<Person> findByLastName(String lastName);
+
+	// public List<Person> findByEducationalStatusChildrens(String
+	// educationalStatus);
+
+	public boolean delete(long id);
 }
--- src/main/java/mum/edu/foster/service/ChildrenService.java
+++ src/main/java/mum/edu/foster/service/ChildrenService.java
@@ -1,17 +1,29 @@
 package mum.edu.foster.service;
 
+import java.util.Date;
 import java.util.List;
 
+import mum.edu.foster.domain.Children;
 import mum.edu.foster.domain.Person;
 
 public interface ChildrenService {
 
-	public void addChildren();
+	public boolean add();
+
+	public List<Person> getAll();
 
 	public Person getChildrenById(Long id);
 
-	public Person getChildrenByName(String firstName);
+	public List<Person> getChildrenByFistName(String firstName);
 
-	public List<Person> getAll();
+	public List<Person> getChildrenByLastName(String lastName);
+
+	public List<Person> getByEducationslStatus(String educationalStatus);
+
+	public List<Person> getByDateofBirth(Date dob);
+
+	public boolean update(Children children);
+
+	public boolean delete(long id);
 
 }
